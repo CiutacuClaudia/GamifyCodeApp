5aab6becb6ea59ea853cbbfd9e33a72f2823cb56
Claudia Ciutacu <ciutacu51@gmail.com>
2024-01-10 17:40:32 +0200

    delete unnecessary code

--- app/src/main/java/com/example/mobilelearningapp/GamifyCodeApp.kt
+++ app/src/main/java/com/example/mobilelearningapp/GamifyCodeApp.kt
@@ -5,11 +5,4 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GamifyCodeApp : Application() {
-//    override fun onCreate() {
-//        super.onCreate()
-//
-//        // Intentionally cause a NullPointerException
-//        val crashThis: String? = null
-//        val length = crashThis!!.length
-//    }
 }
