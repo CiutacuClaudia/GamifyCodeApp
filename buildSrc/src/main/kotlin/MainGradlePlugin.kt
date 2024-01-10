aa492b9e484b458e72d1e241ed8ae88e627adddc
Claudia Ciutacu <ciutacu51@gmail.com>
2024-01-10 17:29:20 +0200

    Clean Code

--- buildSrc/src/main/kotlin/MainGradlePlugin.kt
+++ buildSrc/src/main/kotlin/MainGradlePlugin.kt
@@ -1,8 +1,6 @@
 import com.android.build.gradle.LibraryExtension
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.provideDelegate
 
 class MainGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
